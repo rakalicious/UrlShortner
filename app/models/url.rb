21a1739e74a339ab666e7bbe4430aad718e4d3ed
rakalicious <rakalicious2408@gmail.com>
2019-01-25 10:19:17 +0530

    all error by wrong table done

--- app/models/url.rb
+++ app/models/url.rb
@@ -9,34 +9,42 @@ class Url < ApplicationRecord
 		CounterWorker.perform_async
 	end
 
-	def random_string_for_domain(number)
+	def self.random_string_for_domain(number)
 		possible_domain = UrlsHelper.random_n_string(4,number)
-		while Domain.find_by(short_domain: possible_domain) != null do
+		while Domain.find_by(short_domain: possible_domain) != nil do
 			possible_domain = UrlsHelper.random_n_string(4,number)
 		end
+		return possible_domain
 	end
 
-	def get_random_string_for_url
+	def self.random_string_for_url(number)
 		possible_short = UrlsHelper.random_n_string(7,number)
 		while Url.find_by(short_url: possible_short) != nil do
 			possible_short = UrlsHelper.random_n_string(7,number)
 		end
+		return possible_short
 	end
 
 	def self.shorten_url(long_url , long_domain)
 		number = 62
 		if Url.find_by(long_url: long_url) == nil
 			short_domain = Url.find_short_domain(long_domain)
-			possible_short = random_string_for_url(number)
-			
+			possible_short = Url.random_string_for_url(number)
+			#puts short_domain
+			#puts possible_short
 
 			@short_url = Url.create({:long_url => long_url, :short_url => possible_short , :short_domain => short_domain})
 			#@urls.save
 			return (short_domain)+"/"+(Url.find_by(long_url: long_url).short_url)
 		else
+			#short_domain = Url.find_by(long_url: long_url).short_domain
+			#actual_domain = Domain.find_by(short_domain: short_domain).domain_name
 			@short_url = Rails.cache.fetch(long_url , :expires_in => 5.minutes) do
-
-				(Url.where(long_url: long_url).first.short_domain )+"/" +(Url.where(long_url: long_url).first.short_url)
+				a = (Url.where(long_url: long_url).first.short_domain )
+				puts a
+				b = (Url.where(long_url: long_url).first.short_url)
+				puts b
+				a+"/" +b
 				end
 			return @short_url
 		end
@@ -69,7 +77,7 @@ class Url < ApplicationRecord
 		number = 52
 		#Rails.cache.clear
 		if Domain.find_by(domain_name: long_domain) == nil
-			possible_domain = random_string_for_domain(number)
+			possible_domain = Url.random_string_for_domain(number)
 			@short_domain = Domain.create({:domain_name => long_domain, :short_domain => possible_domain})
 			#@domains.save
 			return Domain.find_by(domain_name: long_domain).short_domain
